af357107cce4db2a019c2a9f5f19495ee5040a50
aditiy1 <70488412+aditiy1@users.noreply.github.com>
2022-07-18 17:05:35 +0000

    created lists and tuple and appended and inserted

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-name="Aditi"
-print('Hello '+name)
+# name="Aditi"
+# print('Hello '+name)
+
+#create list
+# fruits = ["apple", "orange", "banana", "cherry"]
+
+# print(fruits)
+# #add watermelon to list fruits
+# fruits.append("watermelon")
+# print(fruits)
+# print(fruits[0])
+# print(fruits[3])
+# print(fruits[-1])
+# fruits.insert(1, "pineapple")
+# print(fruits)
+# print(len(fruits))
+
+# for i in range(len(fruits)):
+#   print(fruits[i])
+
+# for each in fruits:
+#   print(each)
+
+# for value in fruits:
+#   print(value)
+
+# t=(1,2,3,4,5)
+# print(len(t))
+
